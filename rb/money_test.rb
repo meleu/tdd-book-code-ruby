3c6d02d449f951b30684effb89be1a566f012472
meleu <8508804+meleu@users.noreply.github.com>
2024-09-25 17:11:42 -0300

    feat: division and multiplication features done

--- rb/money_test.rb
+++ rb/money_test.rb
@@ -1,22 +1,43 @@
-require "minitest/autorun"
+require 'minitest/autorun'
 
-class Dollar
-  attr_reader :amount
+class Money
+  attr_reader :amount, :currency
 
-  def initialize(amount)
+  def initialize(amount, currency)
     @amount = amount
+    @currency = currency
   end
 
   def times(multiplier)
-    Dollar.new(amount * multiplier)
+    Money.new(amount * multiplier, currency)
+  end
+
+  def divide(divisor)
+    Money.new(amount.to_f / divisor, currency)
+  end
+
+  def ==(other)
+    amount == other.amount && currency == other.currency
   end
 end
 
 class MoneyTest < Minitest::Test
-  def test_multiplication
-    fiver = Dollar.new(5)
-    tenner = fiver.times(2)
+  def test_multiplication_in_dollars
+    five_dollars = Money.new(5, 'USD')
+    ten_dollars = Money.new(10, 'USD')
+    assert_equal ten_dollars, five_dollars.times(2)
+  end
 
-    assert_equal 10, tenner.amount
+  def test_multiplication_in_euros
+    ten_euros = Money.new(10, 'EUR')
+    twenty_euros = Money.new(20, 'EUR')
+    assert_equal twenty_euros, ten_euros.times(2)
+  end
+
+  def test_division
+    original_money = Money.new(4002, 'KRW')
+    actual_money_after_division = original_money.divide(4)
+    expected_money_after_division = Money.new(1000.5, 'KRW')
+    assert_equal expected_money_after_division, actual_money_after_division
   end
 end
